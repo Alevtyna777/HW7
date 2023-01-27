2a1aacd5593ec36be5184eb5edec936b9476e417
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-01-27 21:22:01 +0200

    homework # 7.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
+
     public static void main(String[] args) {
 
         Random random = new Random();
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome! Choose from  1 to 10. You have minimum 3 attempts");
         int secretNumber = random.nextInt(10);
         int counter = 0;
         boolean isRunning = true;
@@ -13,14 +16,17 @@ public class Main {
         while (isRunning)  {
             System.out.println("Type your number:");
             int number = scanner.nextInt();
-            if (number == secretNumber)
-            {
+            counter++;
+            isRunning = counter >= 3 ;
+
+            if (number == secretNumber) {
                 System.out.println("WIn!");
                 isRunning = false;
+            } else if (secretNumber != number) {
+                System.out.println("Lose number  = " + secretNumber);
             }
-            counter ++ ;
-            isRunning = counter >= 3 ;
         }
-        System.out.println("Lose number " + secretNumber);
+
+        System.out.println(" Congrats! Game is over! ");
     }
 }
